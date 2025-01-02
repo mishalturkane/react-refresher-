34ab5ecfdfd40a4c0c81f907da7a88ab3b8dcbd2
user.name: mishalturkane <mishalturkane@gmail.com>
2025-01-03 00:14:33 +0530

    first react app created

---
+++ 01reactapp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+    <code>Hey Budy this is the first react app created by the create-react-app</code>
+    </div>
+  );
+}
+
+export default App;
